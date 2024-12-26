f0389d512869a6f3430fb3fe8736c2d8482ad1fa
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-12-25 23:03:46 -0300

    fet: mapeamento dos botões da interfaace

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-console.log("teste")
+const input = document.querySelector("#input") as HTMLInputElement
+const igualdade = document.querySelector("#igualdade") as HTMLButtonElement
+const apagar = document.querySelector("#apagar") as HTMLButtonElement
+
+const numeros = [... document.querySelectorAll(".numeros")] as Array<HTMLButtonElement>
+const operadores = [... document.querySelectorAll(".operadores")] as Array<HTMLButtonElement>
+
+igualdade.addEventListener("click", () => {
+    
+})
+
+apagar.addEventListener("click", () => {
+    
+})
+
+numeros.map(numero => numero.addEventListener("click", e => {
+
+}))
+
+operadores.map(operador => operador.addEventListener("click", () => {
+    
+}))
